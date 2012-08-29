3ccbff0ccff0fcc6322d31406cdccea7f28eea5e
emilybache <emily@bacheconsulting.com>
2012-08-29 10:42:46 +0200

    more pythonic method name

--- TDDMicroExercises/python/TurnTicketDispenser/turn_ticket.py
+++ TDDMicroExercises/python/TurnTicketDispenser/turn_ticket.py
@@ -6,13 +6,13 @@ class TurnNumberSequence(object):
     _turnNumber = -1
 
     @staticmethod
-    def getNextTurnNumber():
+    def next_turn_number():
         TurnNumberSequence._turnNumber += 1
         return TurnNumberSequence._turnNumber
 
 
 class TicketDispenser(object):
     def getTurnTicket(self):
-        newTurnNumber = TurnNumberSequence.getNextTurnNumber()
+        newTurnNumber = TurnNumberSequence.next_turn_number()
         newTurnTicket = TurnTicket(newTurnNumber)
         return newTurnTicket
