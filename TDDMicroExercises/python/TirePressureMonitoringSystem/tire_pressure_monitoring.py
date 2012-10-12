f0a052a55f22b400b354888913cfab26cb8cbc9a
emilybache <emily@bacheconsulting.com>
2012-10-12 15:14:54 +0200

    make is_alarm_on into a property to protect it from being written to. Thanks to neppord for the suggestion

--- TDDMicroExercises/python/TirePressureMonitoringSystem/tire_pressure_monitoring.py
+++ TDDMicroExercises/python/TirePressureMonitoringSystem/tire_pressure_monitoring.py
@@ -21,11 +21,13 @@ class Alarm(object):
         self._low_pressure_threshold = 17
         self._high_pressure_threshold = 21
         self._sensor = Sensor()
-        self.is_alarm_on = False
-        self._alarm_count = 0
-
+        self._is_alarm_on = False
+        
     def check(self):
         psi_pressure_value = self._sensor.pop_next_pressure_psi_value()
         if psi_pressure_value < self._low_pressure_threshold or self._high_pressure_threshold < psi_pressure_value:
-            self.is_alarm_on = True
-            self._alarm_count += 1
+            self._is_alarm_on = True
+
+    @property
+    def is_alarm_on(self):
+        return self._is_alarm_on
