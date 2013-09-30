07c0e2dbdab85acc89a171f0846c165f1a76aa3b
emilybache <emily@bacheconsulting.com>
2013-09-30 09:32:55 +0200

    added Java version of slightly harder text converter exercise

---
+++ Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlPagesConverter.java
@@ -0,0 +1,54 @@
+package tddmicroexercises.textconvertor;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
+public class HtmlPagesConverter {
+
+	private String filename;
+	private List<Integer> breaks = new ArrayList<Integer>();
+	
+	public HtmlPagesConverter(String filename) throws IOException {
+		this.filename = filename;
+
+		this.breaks.add(0);
+		BufferedReader reader = new BufferedReader(new FileReader(this.filename));
+		int cumulativeCharCount = 0;
+	    String line = reader.readLine();
+	    while (line != null)
+	    {
+	    	cumulativeCharCount += line.length() + 1; // add one for the newline
+	    	if (line.contains("PAGE_BREAK")) {
+	    		int page_break_position = cumulativeCharCount;
+	    		breaks.add(page_break_position);
+	    	}
+	    	line = reader.readLine();
+	    }
+	    reader.close();
+	}
+
+	public String getHtmlPage(int page) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(this.filename));
+	    reader.skip(breaks.get(page));
+	    StringBuffer htmlPage = new StringBuffer();
+	    String line = reader.readLine();
+	    while (line != null)
+	    {
+	    	if (line.contains("PAGE_BREAK")) {
+	    		break;
+	    	}
+	    	htmlPage.append(StringEscapeUtils.escapeHtml4(line));
+	    	htmlPage.append("<br />");
+	        
+	    	line = reader.readLine();
+	    }
+	    reader.close();
+	    return htmlPage.toString();
+	}
+	
+}
