ab4d80bfc4ccc01181d1a253f94ae8eceb603b6f
Hemal Varambhia <h.n.varambhia@gmail.com>
2015-05-25 16:49:13 +0100

    Syntax error corrected, escapeHTML takes 1 arg, initialise html string outside and return it

--- Ruby/TextConverter/unicode_to_html_converter.rb
+++ Ruby/TextConverter/unicode_to_html_converter.rb
@@ -1,19 +1,21 @@
 require 'cgi'
 
 class UnicodeFileToHtmlTextConverter
-
-  def initialize(full_filename_with_path):
+  attr_accessor :full_filename_with_path
+  def initialize(full_filename_with_path)
     @full_filename_with_path = full_filename_with_path
   end
 
   def convert_to_html
+    html = ""
     File.open(self.full_filename_with_path, "r") do |f|
-      html = ""
       while (line = f.gets)
           line = line.chomp()
-          html += CGI.escapeHTML(line, quote=True)
+          html += CGI.escapeHTML(line)
           html += "<br />"
       end
-      html
+    end
+
+    return html
   end
 end
