a153875e06b673dce7fad0aa46b7daf9917571c2
Peter Kofler <peter.kofler@code-cop.org>
2015-06-07 22:20:49 +0200

    format Java source so it is consistent with other Java sources (braces on new line), reorder constructor first, no code changes

--- Java/Leaderboard/src/main/java/tddmicroexercises/leaderboard/Driver.java
+++ Java/Leaderboard/src/main/java/tddmicroexercises/leaderboard/Driver.java
@@ -1,20 +1,24 @@
 package tddmicroexercises.leaderboard;
 
-public class Driver {
+public class Driver
+{
 
     private final String name;
     private final String country;
 
-    public Driver(String name, String country) {
+    public Driver(String name, String country)
+    {
         this.name = name;
         this.country = country;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public String getCountry() {
+    public String getCountry()
+    {
         return country;
     }
 }
--- Java/Leaderboard/src/main/java/tddmicroexercises/leaderboard/Leaderboard.java
+++ Java/Leaderboard/src/main/java/tddmicroexercises/leaderboard/Leaderboard.java
@@ -1,24 +1,37 @@
 package tddmicroexercises.leaderboard;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
-public class Leaderboard {
+public class Leaderboard
+{
 
     private final List<Race> races;
 
-    public Leaderboard(Race... races) {
+    public Leaderboard(Race... races)
+    {
         this.races = Arrays.asList(races);
     }
 
-    public Map<String, Integer> driverResults() {
+    public Map<String, Integer> driverResults()
+    {
         Map<String, Integer> results = new HashMap<String, Integer>();
-        for (Race race: this.races) {
-            for (Driver driver: race.getResults()) {
+        for (Race race : this.races)
+        {
+            for (Driver driver : race.getResults())
+            {
                 String driverName = race.getDriverName(driver);
                 int points = race.getPoints(driver);
-                if (results.containsKey(driverName)) {
+                if (results.containsKey(driverName))
+                {
                     results.put(driverName, results.get(driverName) + points);
-                } else {
+                } else
+                {
                     results.put(driverName, race.getPoints(driver));
                 }
             }
@@ -26,10 +39,12 @@ public class Leaderboard {
         return results;
     }
 
-    public List<String> driverRankings() {
+    public List<String> driverRankings()
+    {
         Map<String, Integer> results = driverResults();
-        TreeMap<Integer,String> sortedResults = new TreeMap<Integer,String>();
-        for (String driverName: results.keySet()) {
+        Map<Integer, String> sortedResults = new TreeMap<Integer, String>();
+        for (String driverName : results.keySet())
+        {
             sortedResults.put(results.get(driverName), driverName);
         }
         List<String> resultsList = new ArrayList<String>(sortedResults.values());
