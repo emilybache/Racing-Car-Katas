f859464a0485d66d7806996f8680b329ff54baa3
Peter Kofler <peter.kofler@code-cop.org>
2015-06-11 14:34:43 +0200

    fix Bug in Java, do not drop drivers with same points

--- Java/Leaderboard/src/main/java/tddmicroexercises/leaderboard/Leaderboard.java
+++ Java/Leaderboard/src/main/java/tddmicroexercises/leaderboard/Leaderboard.java
@@ -3,10 +3,10 @@ package tddmicroexercises.leaderboard;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Leaderboard
 {
@@ -30,10 +30,9 @@ public class Leaderboard
                 if (results.containsKey(driverName))
                 {
                     results.put(driverName, results.get(driverName) + points);
-                } 
-                else
+                } else
                 {
-                    results.put(driverName, race.getPoints(driver));
+                    results.put(driverName, 0 + points);
                 }
             }
         }
@@ -43,13 +42,25 @@ public class Leaderboard
     public List<String> driverRankings()
     {
         Map<String, Integer> results = driverResults();
-        Map<Integer, String> sortedResults = new TreeMap<>();
-        for (String driverName : results.keySet())
-        {
-            sortedResults.put(results.get(driverName), driverName);
-        }
-        List<String> resultsList = new ArrayList<>(sortedResults.values());
-        Collections.reverse(resultsList);
+        List<String> resultsList = new ArrayList<>(results.keySet());
+        Collections.sort(resultsList, new DriverByPointsDescendingComparator(results));
         return resultsList;
     }
+
+    private static final class DriverByPointsDescendingComparator implements Comparator<String>
+    {
+        private final Map<String, Integer> results;
+
+        private DriverByPointsDescendingComparator(Map<String, Integer> results)
+        {
+            this.results = results;
+        }
+
+        @Override
+        public int compare(String driverName1, String driverName2)
+        {
+            return -results.get(driverName1).compareTo(results.get(driverName2));
+        }
+    }
+
 }
