0ac1d938c6295887467e73250db48e81c733fbed
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-07-15 21:28:44 +0100

    Trnslated from the python

--- Ruby/LeaderBoard/leader_board.rb
+++ Ruby/LeaderBoard/leader_board.rb
@@ -5,31 +5,20 @@ class LeaderBoard
     @races = races
   end
 
-  def driver_results
-    results = {}
+  def driver_points
+    driver_points = {}
     @races.each do |race|
       race.results.each do |driver|
-        driver_name = race.driver_name driver
-	points = race.points driver
-	if results.has_key?(driver_name)
-	  results[driver_name] = results[driver_name] + points
-	else
-	  results[driver_name] = race.points driver
-	end
+        name = race.driver_name(driver)
+        driver_points[name] += race.points(driver) 
       end
     end
-    
-    return results
+
+    return driver_points
   end
 
   def driver_rankings
-    results = driver_results
-    sorted_results = {}
-    results.each_key do |driver_name|
-      sorted_results[results[driver_name]] = driver_name
-    end
-    results_list = sorted_results.values
-
-    return results_list.reverse
+    rankings = driver_points.sort_by{|name, points| points}.reverse
+    return rankings.collect{|name, points| names}
   end
 end
