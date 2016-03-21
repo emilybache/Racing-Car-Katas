94d204595515d325b0dc84105c7bd06f9f0dfaf3
andrei <andrey.chernishov@gmail.com>
2016-03-21 20:04:34 +0100

    tests were splitted  for setup-act-verify sections

--- Java/Leaderboard/src/test/java/tddmicroexercises/leaderboard/LeaderboardTest.java
+++ Java/Leaderboard/src/test/java/tddmicroexercises/leaderboard/LeaderboardTest.java
@@ -16,24 +16,39 @@ public class LeaderboardTest {
 
     @Test
     public void itShouldSumThePoints() {
+        // setup
+
+        // act
         Map<String, Integer> results = TestData.sampleLeaderboard1.driverResults();
+
+        // verify
         assertTrue("results " + results, results.containsKey("Lewis Hamilton"));
         assertEquals(18 + 18 + 25, (int) results.get("Lewis Hamilton"));
     }
 
     @Test
     public void isShouldFindTheWinner() {
-        assertEquals("Lewis Hamilton", TestData.sampleLeaderboard1.driverRankings().get(0));
+        // setup
+
+        // act
+        List<String> result = TestData.sampleLeaderboard1.driverRankings();
+
+        // verify
+        assertEquals("Lewis Hamilton", result.get(0));
     }
 
     @Test
     public void itShouldKeepAllDriversWhenSamePoints() {
+        // setup
         // bug, drops drivers with same points
         Race winDriver1 = new Race("Australian Grand Prix", driver1, driver2, driver3);
         Race winDriver2 = new Race("Malaysian Grand Prix", driver2, driver1, driver3);
         Leaderboard exEquoLeaderBoard = new Leaderboard(winDriver1, winDriver2);
 
+        // act
         List<String> rankings = exEquoLeaderBoard.driverRankings();
+
+        // verify
         assertEquals(Arrays.asList(driver1.getName(), driver2.getName(), driver3.getName()), rankings);
         // note: the order of driver1 and driver2 is JDK/platform dependent
     }
--- Java/Leaderboard/src/test/java/tddmicroexercises/leaderboard/RaceTest.java
+++ Java/Leaderboard/src/test/java/tddmicroexercises/leaderboard/RaceTest.java
@@ -7,6 +7,11 @@ public class RaceTest {
 
     @Test
     public void isShouldCalculateDriverPoints() {
+        // setup
+
+        // act
+
+        // verify
         Assert.assertEquals(25, TestData.race1.getPoints(TestData.driver1));
         Assert.assertEquals(18, TestData.race1.getPoints(TestData.driver2));
         Assert.assertEquals(15, TestData.race1.getPoints(TestData.driver3));
