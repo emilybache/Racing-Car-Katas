0bc45d5d8d4b9faedeadb3b5b1589f1cec2a597b
Hossein Taleghani <sh.taleghani@gmail.com>
2020-04-24 15:00:40 +0430

    add TextConverter

---
+++ php/src/TextConverter/HtmlPagesConverter.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace RacingCar\TextConverter;
+
+/**
+ * Class HtmlPages
+ * This is a second, slightly harder problem on the same theme as the UnicodeFileToHtmlTextConverter
+ *
+ * @package RacingCar\TextConverter
+ */
+class HtmlPagesConverter
+{
+    private string $filename;
+    private array $breaks;
+
+    /**
+     * HtmlPages constructor.
+     * Reads the file and note the positions of the page breaks so we can access them quickly
+     * @param string $filename
+     */
+    public function __construct(string $filename)
+    {
+        $this->filename = $filename;
+        $this->breaks = [0];
+        $f = fopen($this->filename, 'r');
+        while ($line = fgets($f) !== false)
+        {
+            $line = rtrim($line);
+            if (strpos($line, 'PAGE_BREAK') !== false) {
+                $pageBreakPosition = ftell($f);
+                $this->breaks[] = ftell($f);
+            }
+        }
+        $this->breaks[] = ftell($f);
+        fclose($f);
+    }
+
+    /**
+     * @param int $page Page number (zero index)
+     * @return string HTML page with the given number
+     */
+    public function getHtmlPage(int $page): string
+    {
+        $pageStart = $this->breaks[$page];
+        $pageEnd = $this->breaks[$page + 1];
+        $html = "";
+        $f = fopen($this->filename, 'r');
+        fseek($f, $pageStart);
+        while (ftell($f) != $pageEnd) {
+            $line = rtrim(fgets($f));
+            if (strpos($line, 'PAGE_BREAK') !== false) {
+                continue;
+            }
+            $html .= htmlspecialchars($line, ENT_QUOTES | ENT_HTML5);
+            $html .= "<br />";
+        }
+        fclose($f);
+        return $html;
+    }
+}
