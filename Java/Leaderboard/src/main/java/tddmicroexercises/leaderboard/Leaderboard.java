97596903e627d0477198c366346ff68e193342db
Nemanja Spajic <nemanja.spajic@doodle.com>
2022-04-04 11:06:30 +0200

    feat: new tests and refactoring

--- Java/Leaderboard/src/main/java/tddmicroexercises/leaderboard/Leaderboard.java
+++ Java/Leaderboard/src/main/java/tddmicroexercises/leaderboard/Leaderboard.java
@@ -1,55 +1,32 @@
 package tddmicroexercises.leaderboard;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Map.Entry.comparingByValue;
 
 public class Leaderboard {
 
-    private final List<Race> races;
+  private final List<Race> races;
 
-    public Leaderboard(Race... races) {
-        this.races = Arrays.asList(races);
+  public Leaderboard(Race... races) {
+    this.races = Arrays.asList(races);
+  }
+
+  public Map<String, Integer> driverResults() {
+    Map<String, Integer> results = new HashMap<>();
+    for (Race race : this.races) {
+      for (Driver driver : race.getResults()) {
+        String driverName = race.getDriverName(driver);
+        results.put(driverName, results.getOrDefault(driverName, 0) + race.getPoints(driver));
+      }
     }
+    return results;
+  }
 
-    public Map<String, Integer> driverResults() {
-        Map<String, Integer> results = new HashMap<>();
-        for (Race race : this.races) {
-            for (Driver driver : race.getResults()) {
-                String driverName = race.getDriverName(driver);
-                int points = race.getPoints(driver);
-                if (results.containsKey(driverName)) {
-                    results.put(driverName, results.get(driverName) + points);
-                } else {
-                    results.put(driverName, 0 + points);
-                }
-            }
-        }
-        return results;
-    }
-
-    public List<String> driverRankings() {
-        Map<String, Integer> results = driverResults();
-        List<String> resultsList = new ArrayList<>(results.keySet());
-        Collections.sort(resultsList, new DriverByPointsDescendingComparator(results));
-        return resultsList;
-    }
-
-    private static final class DriverByPointsDescendingComparator implements Comparator<String> {
-        private final Map<String, Integer> results;
-
-        private DriverByPointsDescendingComparator(Map<String, Integer> results) {
-            this.results = results;
-        }
-
-        @Override
-        public int compare(String driverName1, String driverName2) {
-            return -results.get(driverName1).compareTo(results.get(driverName2));
-        }
-    }
-
+  public List<String> driverRankings() {
+    ArrayList<Map.Entry<String, Integer>> entries = new ArrayList<>(driverResults().entrySet());
+    entries.sort(comparingByValue(Comparator.reverseOrder()));
+    return entries.stream().map(Map.Entry::getKey).collect(Collectors.toList());
+  }
 }
--- Java/Leaderboard/src/test/java/tddmicroexercises/leaderboard/LeaderboardTest.java
+++ Java/Leaderboard/src/test/java/tddmicroexercises/leaderboard/LeaderboardTest.java
@@ -1,56 +1,201 @@
 package tddmicroexercises.leaderboard;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static tddmicroexercises.leaderboard.TestData.driver1;
-import static tddmicroexercises.leaderboard.TestData.driver2;
-import static tddmicroexercises.leaderboard.TestData.driver3;
+import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LeaderboardTest {
+  private static final Driver MARCO = new Driver("Marco", "Italy");
+  private static final Driver ALEX = new Driver("Alex", "Austria");
+  private static final Driver STEFAN = new Driver("Stefan", "Serbia");
+  private static final Driver NEMANJA = new Driver("Nemanja", "Serbia");
 
-    @Test
-    public void itShouldSumThePoints() {
-        // setup
+  @Test
+  void driverRankings_givenOneRace_thenDriversShouldHaveExpectedPositions() {
+    Race race = new Race("Mugello", MARCO, ALEX, STEFAN);
+    Leaderboard sut = new Leaderboard(race);
 
-        // act
-        Map<String, Integer> results = TestData.sampleLeaderboard1.driverResults();
+    List<String> result = sut.driverRankings();
 
-        // verify
-        assertTrue(results.containsKey("Lewis Hamilton"), "results " + results);
-        assertEquals(18 + 18 + 25, (int) results.get("Lewis Hamilton"));
-    }
+    assertThat(result).hasSize(3);
+    assertThat(result.get(0)).isEqualTo(MARCO.getName());
+    assertThat(result.get(1)).isEqualTo(ALEX.getName());
+    assertThat(result.get(2)).isEqualTo(STEFAN.getName());
+  }
 
-    @Test
-    public void isShouldFindTheWinner() {
-        // setup
+  @Test
+  void driverRankings_noRaceGiven_thenNoDrivers() {
+    Leaderboard sut = new Leaderboard();
 
-        // act
-        List<String> result = TestData.sampleLeaderboard1.driverRankings();
+    List<String> result = sut.driverRankings();
 
-        // verify
-        assertEquals("Lewis Hamilton", result.get(0));
-    }
+    assertThat(result).isEmpty();
+  }
 
-    @Test
-    public void itShouldKeepAllDriversWhenSamePoints() {
-        // setup
-        // bug, drops drivers with same points
-        Race winDriver1 = new Race("Australian Grand Prix", driver1, driver2, driver3);
-        Race winDriver2 = new Race("Malaysian Grand Prix", driver2, driver1, driver3);
-        Leaderboard exEquoLeaderBoard = new Leaderboard(winDriver1, winDriver2);
+  @Test
+  void driverRankings_givenOneDriver_thenResultHasSizeOfOne() {
+    Race race = new Race("Mugello", MARCO);
+    Leaderboard sut = new Leaderboard(race);
 
-        // act
-        List<String> rankings = exEquoLeaderBoard.driverRankings();
+    List<String> result = sut.driverRankings();
 
-        // verify
-        assertEquals(Arrays.asList(driver1.getName(), driver2.getName(), driver3.getName()), rankings);
-        // note: the order of driver1 and driver2 is JDK/platform dependent
-    }
+    assertThat(result).hasSize(1);
+    assertThat(result.get(0)).isEqualTo(MARCO.getName());
+  }
 
+  @Test
+  void driverRankings_givenMoreThan3_thenShowFirst3() {
+    Race race = new Race("Mugello", MARCO, ALEX, STEFAN, NEMANJA);
+    Leaderboard sut = new Leaderboard(race);
+
+    assertThrows(ArrayIndexOutOfBoundsException.class, sut::driverRankings);
+  }
+
+  @Test
+  void driverRankings_givenTwoRacesWithSameOrder_thenDriversShouldHaveExpectedPositions() {
+    Race raceMugello = new Race("Mugello", MARCO, ALEX, STEFAN);
+    Race raceMisano = new Race("Misano", MARCO, ALEX, STEFAN);
+    Leaderboard sut = new Leaderboard(raceMugello, raceMisano);
+
+    List<String> result = sut.driverRankings();
+
+    assertThat(result).hasSize(3);
+    assertThat(result.get(0)).isEqualTo(MARCO.getName());
+    assertThat(result.get(1)).isEqualTo(ALEX.getName());
+    assertThat(result.get(2)).isEqualTo(STEFAN.getName());
+  }
+
+  @Test
+  void
+      driverRankings_givenTwoRacesWithTwoDifferentWinners_thenDriversShouldHaveExpectedPositions() {
+    Race raceMugello = new Race("Mugello", MARCO);
+    Race raceMisano = new Race("Misano", ALEX);
+    Leaderboard sut = new Leaderboard(raceMugello, raceMisano);
+
+    List<String> result = sut.driverRankings();
+
+    // note: the order of MARCO and ALEX is JDK/platform dependent
+    assertThat(result).hasSize(2);
+    assertThat(result.get(0)).isEqualTo(MARCO.getName());
+    assertThat(result.get(1)).isEqualTo(ALEX.getName());
+  }
+
+  @Test
+  void
+      driverRankings_givenTwoRacesWithTwoDifferentWinnersAnotherOrder_thenDriversShouldHaveExpectedPositions() {
+    Race raceMugello = new Race("Mugello", MARCO);
+    Race raceMisano = new Race("Misano", ALEX);
+    Leaderboard sut = new Leaderboard(raceMisano, raceMugello);
+
+    List<String> result = sut.driverRankings();
+
+    // note: the order of MARCO and ALEX is JDK/platform dependent
+    assertThat(result).hasSize(2);
+    assertThat(result.get(0)).isEqualTo(MARCO.getName());
+    assertThat(result.get(1)).isEqualTo(ALEX.getName());
+  }
+
+  @Test
+  void driverRankings_givenTwoRacesWithRandomOrder_thenDriversShouldHaveExpectedPositions() {
+    Race raceMugello = new Race("Mugello", MARCO, ALEX, STEFAN);
+    Race raceNavak = new Race("Navak", STEFAN, MARCO, ALEX);
+    Leaderboard sut = new Leaderboard(raceMugello, raceNavak);
+
+    List<String> result = sut.driverRankings();
+
+    assertThat(result).hasSize(3);
+    assertThat(result.get(0)).isEqualTo(MARCO.getName());
+    assertThat(result.get(1)).isEqualTo(STEFAN.getName());
+    assertThat(result.get(2)).isEqualTo(ALEX.getName());
+  }
+
+  @Test
+  void driverRankings_givenTwoRacesWithFourDrivers_thenDriversShouldHaveExpectedPositions() {
+    Race raceMugello = new Race("Mugello", MARCO, ALEX, STEFAN);
+    Race raceNavak = new Race("Navak", STEFAN, NEMANJA, ALEX);
+    Leaderboard sut = new Leaderboard(raceMugello, raceNavak);
+
+    List<String> result = sut.driverRankings();
+
+    assertThat(result).hasSize(4);
+    assertThat(result.get(0)).isEqualTo(STEFAN.getName());
+    assertThat(result.get(1)).isEqualTo(ALEX.getName());
+    assertThat(result.get(2)).isEqualTo(MARCO.getName());
+    assertThat(result.get(3)).isEqualTo(NEMANJA.getName());
+  }
+
+  @Test
+  void driverResults_givenOneDriverAndOneRace_thenDriverPointsEqualTo25() {
+    Race race = new Race("Mugello", MARCO);
+    Leaderboard sut = new Leaderboard(race);
+
+    Map<String, Integer> result = sut.driverResults();
+
+    assertThat(result).containsEntry(MARCO.getName(), 25);
+  }
+
+  @Test
+  void driverResults_givenTwoDriversAndOneRace_thenDriverPointsShouldBeAsExpected() {
+    Race race = new Race("Mugello", MARCO, ALEX);
+    Leaderboard sut = new Leaderboard(race);
+
+    Map<String, Integer> result = sut.driverResults();
+
+    assertThat(result).containsEntry(MARCO.getName(), 25);
+    assertThat(result).containsEntry(ALEX.getName(), 18);
+  }
+
+  @Test
+  void driverResults_givenThreeDriversAndOneRace_thenDriverPointsShouldBeAsExpected() {
+    Race race = new Race("Navak", STEFAN, MARCO, ALEX);
+    Leaderboard sut = new Leaderboard(race);
+
+    Map<String, Integer> result = sut.driverResults();
+
+    assertThat(result).containsEntry(STEFAN.getName(), 25);
+    assertThat(result).containsEntry(MARCO.getName(), 18);
+    assertThat(result).containsEntry(ALEX.getName(), 15);
+  }
+
+  @Test
+  void driverResults_givenThreeDriversAndTwoRaces_thenDriverPointsShouldBeAsExpected() {
+    Race raceNavak = new Race("Navak", STEFAN, MARCO, ALEX);
+    Race raceMugello = new Race("Mugello", MARCO, ALEX, STEFAN);
+
+    Leaderboard sut = new Leaderboard(raceNavak, raceMugello);
+
+    Map<String, Integer> result = sut.driverResults();
+
+    assertThat(result).containsEntry(STEFAN.getName(), 25 + 15);
+    assertThat(result).containsEntry(MARCO.getName(), 18 + 25);
+    assertThat(result).containsEntry(ALEX.getName(), 15 + 18);
+  }
+
+  @Test
+  void driverResults_givenFourDriversAndTwoRaces_thenDriverPointsShouldBeAsExpected() {
+    Race raceNavak = new Race("Navak", STEFAN, MARCO, ALEX);
+    Race raceMugello = new Race("Mugello", MARCO, ALEX, NEMANJA);
+
+    Leaderboard sut = new Leaderboard(raceNavak, raceMugello);
+
+    Map<String, Integer> result = sut.driverResults();
+
+    assertThat(result).containsEntry(STEFAN.getName(), 25);
+    assertThat(result).containsEntry(MARCO.getName(), 18 + 25);
+    assertThat(result).containsEntry(ALEX.getName(), 15 + 18);
+    assertThat(result).containsEntry(NEMANJA.getName(), 15);
+  }
+
+  @Test
+  void driverResults_givenLeaderboardWithoutRaces_thenResultShouldBeEmpty() {
+    Leaderboard sut = new Leaderboard();
+
+    Map<String, Integer> result = sut.driverResults();
+
+    assertThat(result).isEmpty();
+  }
 }
