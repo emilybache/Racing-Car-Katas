a5cabaf850ab1b8fc5dbcd41b487dca5bbd22c1b
Jonathan Beyrak-Lev <jbeylev@gmail.com>
2022-06-09 16:39:22 +0300

    Add broken tests

--- Swift/TextConverter/TextConverterTests/TextConverterTests.swift
+++ Swift/TextConverter/TextConverterTests/TextConverterTests.swift
@@ -9,6 +9,9 @@ import XCTest
 @testable import TextConverter
 
 class TextConverterTests: XCTestCase {
-
+    func testFoo() {
+        let converter = HtmlTextConverter(fullFilenameWithPath: "foo")
+        XCTAssertEqual("fixme", converter.fullFilenameWithPath)
+    }
 }
 
