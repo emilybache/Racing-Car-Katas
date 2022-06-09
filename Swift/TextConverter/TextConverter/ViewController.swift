30e3a05f94d9f11876d69d28b432e67b7e681d71
Jonathan Beyrak-Lev <jbeylev@gmail.com>
2022-06-09 15:33:32 +0300

    Add text converter project

---
+++ Swift/TextConverter/TextConverter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TextConverter
+//
+//  Created by Jonathan Beyrak-Lev on 09/06/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Swift/TextConverter/TextConverterTests/TextConverterTests.swift
@@ -0,0 +1,15 @@
+//
+//  TextConverterTests.swift
+//  TextConverterTests
+//
+//  Created by Jonathan Beyrak-Lev on 09/06/2022.
+//
+
+import XCTest
+@testable import TextConverter
+
+class TextConverterTests: XCTestCase {
+
+
+}
+ 
