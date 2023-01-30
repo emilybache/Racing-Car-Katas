401c9bf450a7c97b270cfe5caf024e46753e191e
MeghaV <megha.vaghela@zemosolabs.com>
2023-01-30 17:55:40 +0530

    Test case to checktransmission and get diagnostic response

--- Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
+++ Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -1,13 +1,28 @@
 package tddmicroexercises.telemetrysystem;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class TelemetryDiagnosticControlsTest
+
 {
-    
-	@Test
-    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
-    {
+
+    private TelemetryDiagnosticControls controls;
+
+    @BeforeEach
+    public void setup(){
+        controls = new TelemetryDiagnosticControls();
+    }
+
+
+    @Test
+    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() throws Exception {
+
+        controls.checkTransmission();
+        assertNotNull(controls.getDiagnosticInfo());
     }
 
 }
