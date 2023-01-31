4858eb06d55ceb65fa9177d32034108c03a0450f
Sagar Golhar <sagar.golhar@zemosolabs.com>
2023-01-31 09:57:48 +0530

    Junit added for telemetryDignosticControlTest

--- Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
+++ Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -1,13 +1,29 @@
 package tddmicroexercises.telemetrysystem;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TelemetryDiagnosticControlsTest
 {
-    
-	@Test
-    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
-    {
+
+
+    TelemetryDiagnosticControls objectUnderTest;
+
+    @BeforeAll
+    public void setUp(){
+
+        objectUnderTest = new TelemetryDiagnosticControls();
     }
+
+	@Test
+    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() throws Exception {
+        objectUnderTest.checkTransmission();
+        Assertions.assertNotNull(objectUnderTest.getDiagnosticInfo());
+    }
+
 
 }
