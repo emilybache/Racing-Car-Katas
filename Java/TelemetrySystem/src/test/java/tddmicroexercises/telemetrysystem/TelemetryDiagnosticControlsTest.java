7d372824f3b8a13d3d8c824868288bc524292df1
Nishitha-gaddam <nishitha.gaddam@zemosolabs.com>
2023-06-16 15:51:57 +0530

    solid katas assignment

--- Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
+++ Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -1,13 +1,45 @@
-package tddmicroexercises.telemetrysystem;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-public class TelemetryDiagnosticControlsTest
-{
-    
-	@Test
-    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
-    {
+public class TelemetryDiagnosticControlsTest {
+    private TelemetryDiagnosticControls telemetryDiagnosticControls;
+    private TelemetryClient telemetryClient;
+
+    @BeforeEach
+    public void prerequisiteForTestDiagnosticControls() {
+        telemetryClient = mock(TelemetryClient.class);
+        telemetryDiagnosticControls = new TelemetryDiagnosticControls(telemetryClient);
     }
+
+    @Test
+    public void testGetDiagnosticInfo() {
+        String expectedDiagnosticInfo = "Diagnostic Information";
+        when(telemetryClient.getDiagnosticInfo()).thenReturn(expectedDiagnosticInfo);
+        String diagnosticInfo = diagnosticControls.getDiagnosticInfo();
+        assertEquals(expectedDiagnosticInfo, diagnosticInfo);
+    }
+
+    @Test
+    public void testCheckTransmissionSuccess() throws ConnectionException, NoMessageReceivedException {
+        when(telemetryClient.getOnlineStatus()).thenReturn(true);
+        when(telemetryClient.receive()).thenReturn("Diagnostic Message");
+        assertDoesNotThrow(() -> diagnosticControls.checkTransmission());
+    }
+
+    @Test
+    public void testCheckTransmissionFailureNoMessageReceivedException()  {
+        when(telemetryClient.getOnlineStatus()).thenReturn(true);
+        when(telemetryClient.receive()).thenReturn(null);
+        assertThrows(NoMessageReceivedException.class, () -> diagnosticControls.checkTransmission());
+    }
+
+    @Test
+    public void testCheckTransmissionFailureConnectionException() {
+        when(telemetryClient.getOnlineStatus()).thenReturn(false);
+        assertThrows(ConnectionException.class, () -> diagnosticControls.checkTransmission());
+    }
+
+
 
 }
