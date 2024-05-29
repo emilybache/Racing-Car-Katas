2ab65e29988dc310813946e7ccb7c859c0f4781a
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2024-05-29 12:05:40 +0200

    Add TelemetrySystem classes and empty test

--- Swift/TelemetrySystem/Sources/main.swift
+++ Swift/TelemetrySystem/Sources/main.swift
@@ -1,4 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-print("Hello, world!")
+let diagnosticControls = TelemetryDiagnosticControls()
+
+do {
+    print("Checking transmission")
+    try diagnosticControls.checkTransmission()
+    print(diagnosticControls.diagnosticInfo)
+} catch {
+    print(error)
+}
