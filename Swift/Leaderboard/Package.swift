0e7d9601a9763d0b13d30f6d14f826d178c9ff7b
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2024-05-30 10:32:09 +0200

    remove main file from Leaderboard project

--- Swift/Leaderboard/Package.swift
+++ Swift/Leaderboard/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
+        .target(
             name: "Leaderboard"
         ),
         .testTarget(
--- Swift/Leaderboard/Sources/main.swift
+++
@@ -1,4 +0,0 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-print("Hello, world!")
