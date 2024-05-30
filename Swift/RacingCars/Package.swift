b2d1506b4fb95ed5b5750438b1e868746ff5bcf2
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2024-05-30 11:04:36 +0200

    Regroup multiple swift packages into a single one with multiple targets, so that the dev has to open only one project

---
+++ Swift/RacingCars/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RacingCars",
+    targets: [
+        .target(name: "TelemetrySystem"),
+        .testTarget(
+            name: "TelemetrySystemTests",
+            dependencies: ["TelemetrySystem"]
+        ),
+        .target(name: "Leaderboard"),
+        .testTarget(
+            name: "LeaderboardTests",
+            dependencies: ["Leaderboard"]
+        )
+    ]
+)
+
