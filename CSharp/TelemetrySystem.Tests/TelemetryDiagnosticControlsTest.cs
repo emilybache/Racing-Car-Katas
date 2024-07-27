3a4fae98eb69a8181bab9dd6486e23d9ac55fd1c
Naresh Kumar <kumarnaresh@microsoft.com>
2024-07-27 14:21:37 +0530

    test if send API is hit when connectd

--- CSharp/TelemetrySystem.Tests/TelemetryDiagnosticControlsTest.cs
+++ CSharp/TelemetrySystem.Tests/TelemetryDiagnosticControlsTest.cs
@@ -20,5 +20,23 @@ namespace TDDMicroExercises.TelemetrySystem.Tests
                 Assert.Same(telemetryClientMock.Object, telemetryDiagnosticControls.TelemetryClient);
             }
         }
+
+        public class CheckTransmissionShould
+        {
+            [Fact]
+            public void Send_a_diagnostic_message_when_connected()
+            {
+                // Arrange
+                var telemetryClientMock = new Mock<ITelemetryClient>();
+                var telemetryDiagnosticControls = new TelemetryDiagnosticControls(telemetryClientMock.Object);
+                telemetryClientMock.Setup(x => x.IsConnected()).Returns(true);
+
+                // Act
+                telemetryDiagnosticControls.CheckTransmission();
+
+                // Assert
+                telemetryClientMock.Verify(x => x.Send(TelemetryClient.DiagnosticMessage), Times.Once);
+            }
+        }
     }
 }
