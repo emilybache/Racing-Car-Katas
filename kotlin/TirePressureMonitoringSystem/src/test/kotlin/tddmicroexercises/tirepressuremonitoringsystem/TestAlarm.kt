815f13de753322a9851ae260de80c419032404e1
Jelle van der Zwaag <vanderzwaag@neozo.de>
2025-05-21 22:17:19 +0200

    Migrate to Kotlin 2
    - Kotlin to 2.1.21
    - Java to 21
    - Gradle to 8.14
    - Junit to 5.12.2

--- kotlin/TirePressureMonitoringSystem/src/test/kotlin/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.kt
+++ kotlin/TirePressureMonitoringSystem/src/test/kotlin/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.kt
@@ -1,7 +1,7 @@
 package tddmicroexercises.tirepressuremonitoringsystem
 
-import org.junit.Test
-import org.junit.Assert.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 class TestAlarm {
 
---
+++ kotlin/TurnTicketDispenser/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "2.1.21"
+}
+
+group = "TurnTicketDispenser"
+version =  "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.12.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
